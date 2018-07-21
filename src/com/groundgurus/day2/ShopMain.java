34e68d7ce4c72ced8e2f23539610c782e9049f46
jhomar abancio <abanciojhomar@gmail.com>
2018-07-21 12:50:26 +0800

    added greeting and removed cake shop

--- src/com/groundgurus/day2/ShopMain.java
+++ src/com/groundgurus/day2/ShopMain.java
@@ -8,13 +8,5 @@ public class ShopMain {
     public static void main(String[] args) {
         Shop wineShop = new WineShop("Juan's Wine Shop",new String[] {"Wine 1", "Wine 2", "Wine 3"},"123ABC, 1st street, Mandaluyong, C", 5, false);
         wineShop.printDetails();
-        
-        Shop cakeShop = new Shop();
-        cakeShop.setName("Maria's Wine Shop");
-        cakeShop.setItems(new String[] {"Cake 1", "Cake 2", "Cake 3"});
-        cakeShop.setAddress("789XYZ, 2nd street, Makati, City");
-        cakeShop.setNumberOfEmployees(1);
-        cakeShop.printDetails();
-
     }
 }
